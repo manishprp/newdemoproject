3b4098f02532a4687c2d4891693801719cde167c
manishprp <mrpurohit80055@gmail.com>
2024-05-31 12:16:23 +0530

    Add: ios splash screen

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React, { useEffect } from 'react';
 import { View, StyleSheet, Text, SafeAreaView } from 'react-native';
-import SplashScreen from 'react-native-splash-screen'
+import SplashScreen from 'react-native-splash-screen';
 
 function App(props) {
   useEffect(()=>{
-    SplashScreen.hide();
-  });
+    SplashScreen?.hide()
+  },[]);
   return (
     <SafeAreaView>
     <View style={styles.container}><Text style={{fontSize:20}}>Is Sansaar ko mera pranaam</Text></View>
